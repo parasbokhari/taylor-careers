8cc7e33da2ab361a2d27a862e112a40cf9a6d6b0
Paras Bokhari <parasbokhari@gmail.com>
2026-07-07 11:50:41 -0500

    testing olivia chat widget

--- app/components/OliviaChatLoader.jsx
+++ app/components/OliviaChatLoader.jsx
@@ -1,36 +1,22 @@
-const OLIVIA_SCRIPT_ID = "olivia-chat-widget";
-const OLIVIA_BASE_URL = "https://olivia.paradox.ai";
-const OLIVIA_KEY = "ntsdvrniivyclwbjiojx";
-const OLIVIA_WIDGET_SRC =
-  "https://dokumfe7mps0i.cloudfront.net/static/site/js/widget-client.js";
-
 const oliviaBootstrap = `
 (function(o,l) {
-  if (document.getElementById('${OLIVIA_SCRIPT_ID}')) return;
-  window.oliviaChatData = window.oliviaChatData || [];
-  window.oliviaChatBaseUrl = o;
-  window.oliviaChatData.push(['setKey', l]);
-  window.oliviaChatData.push(['start']);
-  var apply = document.createElement('script');
-  apply.id = '${OLIVIA_SCRIPT_ID}';
-  apply.type = 'text/javascript';
-  apply.async = true;
-  apply.src = '${OLIVIA_WIDGET_SRC}';
-  apply.onload = function() {
-    console.log('Careers Chat script loaded.');
-  };
-  apply.onerror = function() {
-    console.warn('Careers Chat script failed to load.');
-  };
-  document.body.appendChild(apply);
-})('${OLIVIA_BASE_URL}', '${OLIVIA_KEY}');
+window.oliviaChatData = window.oliviaChatData || [];
+window.oliviaChatBaseUrl = o;
+window.oliviaChatData.push(['setKey', l]);
+window.oliviaChatData.push(['start']);
+var apply = document.createElement('script');
+apply.type = 'text/javascript';
+apply.async = true;
+apply.src = 'https://dokumfe7mps0i.cloudfront.net/static/site/js/widget-client.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(apply, s);
+})('https://olivia.paradox.ai', 'ntsdvrniivyclwbjiojx');
 `;
 
 export default function OliviaChatLoader() {
   return (
     <script
       dangerouslySetInnerHTML={{ __html: oliviaBootstrap }}
-      id="olivia-chat-loader"
       type="text/javascript"
     />
   );
